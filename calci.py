37ad3f24b27dd16cb7e5876628b0dba870c6c2b2
bpratibhakiran-arch <bpratibhakiran@gmail.com>
2026-05-20 12:49:47 +0530

    Add files via upload

--- calci.py
+++ calci.py
@@ -6,6 +6,10 @@ num1 = st.number_input("Enter first no. : ")
 num2 = st.number_input("Enter second no. : ")
 add = st.button("Add")
 sub = st.button("Subtract")
+mul = st.button("Multiply")
+div = st.button("Divide")
+pow = st.button("Power")
+per = st.button("Percentage")
 
 if add:
     result = (num1 + num2)
@@ -13,4 +17,20 @@ if add:
 
 elif sub:
     result = (num1 - num2)
+    st.markdown(f"""Answer = {result}""")
+    
+elif mul:
+    result = (num1 * num2)
+    st.markdown(f"""Answer = {result}""")
+
+elif div:
+    result = (num1 / num2)
+    st.markdown(f"""Answer = {result}""")
+
+elif pow:
+    result = (num1 ** num2)
+    st.markdown(f"""Answer = {result}""")
+
+elif per:
+    result = ((num1 / 100) * num2)
     st.markdown(f"""Answer = {result}""")
